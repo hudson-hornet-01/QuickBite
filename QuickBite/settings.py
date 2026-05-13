9f4831a60f627943027c2e8e064fae5695ee19f7
Vaibhav <vaibhavkumarjak@gmail.com>
2026-05-13 19:59:13 +0530

    Add users app to installed_apps

--- QuickBite/settings.py
+++ QuickBite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UserConfig',
+    'Food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Food',
 ]
 
 MIDDLEWARE = [
