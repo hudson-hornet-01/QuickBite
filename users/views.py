acdd708f69435694fcf8c36eec74dc9bed4f86cb
Vaibhav <vaibhavkumarjak@gmail.com>
2026-05-13 21:20:56 +0530

    Add Sign Up functionality for new users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method=="POST":
+        if form.is_valid():
+            form.save()
+            return redirect('Food:index')
+    context = {
+        'form':form
+    }
+    return render(request,'users/register.html',context) 
