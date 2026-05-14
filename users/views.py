8641d1e87cafd250633251b019b33e61bbc4d322
Vaibhav <vaibhavkumarjak@gmail.com>
2026-05-14 07:42:03 +0530

    Add successful registration message functionality

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 def register(request):
     form = UserCreationForm(request.POST or None)
     if request.method=="POST":
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username},your account has been successfully created')
             return redirect('Food:index')
     context = {
         'form':form
