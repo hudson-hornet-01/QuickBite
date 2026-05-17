f99b54c84d5902952fc35c53532b8ebbeb4cb8e3
Vaibhav <vaibhavkumarjak@gmail.com>
2026-05-17 21:38:56 +0530

    Implement user - item relationship

--- Food/models.py
+++ Food/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
 class Item(models.Model):
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=100)
     item_desc = models.TextField()
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
